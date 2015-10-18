93f72010137ccd4d62423acfc52d6bb90944bdca
Joonas Lume <joonaslume@gmail.com>
2015-10-18 12:57:10 +0300

    Implementeerisin Django REST API TestCaseid

--- ToDoList/tests.py
+++ ToDoList/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from ToDoList.models import Post
 
-# Create your tests here.
+
+class CreatePostTest(APITestCase):
+    def test_post_create(self):
+        url = '/api/posts/'
+        data = {"title": "asd", "description": "sdfsdf"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class GetPostTest(APITestCase):
+    def setUp(self):
+        self.post = Post.objects.create(title='TestCase', description='TestCase')
+
+    def test_post_get(self):
+        url = '/api/posts/' + str(self.post.id) + '/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DeletePostTest(APITestCase):
+    def setUp(self):
+        self.post = Post.objects.create(title='TestCase', description='TestCase')
+
+    def test_post_delete(self):
+        url = '/api/posts/' + str(self.post.id) + '/'
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
